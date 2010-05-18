3695f66a2fc84cccb33337fb874c0ad8c8b698ac
Marak Squires <marak.squires@gmail.com>
2010-05-18 19:59:21 -0400

    continuing to plan out library. removed some fluff, added some stubs.

---
+++ index.js
@@ -0,0 +1,10 @@
+var sys = require('sys');
+
+exports.inflector = require('./lib/inflector');
+exports.number = require('./lib/number');
+exports.date = require('./lib/date');
+exports.time = require('./lib/time');
+exports.currency = require('./lib/currency');
+
+
+sys.puts(JSON.stringify(exports));
