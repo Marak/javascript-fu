ecd255cacbdde119e57556221f57afe0cd57ec41
Marak Squires <marak.squires@gmail.com>
2010-05-19 01:16:45 -0400

    moar tests

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var sys = require('sys');
 
-    
+exports.types = require('./lib/types');
 exports.inflector = require('./lib/inflector');
 exports.number = require('./lib/number');
+exports.string = require('./lib/string');
 exports.date = require('./lib/date');
 exports.time = require('./lib/time');
 exports.currency = require('./lib/currency');
