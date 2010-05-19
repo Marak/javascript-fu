99885241dc31f80447038665120509765f64bbcd
Marak Squires <marak.squires@gmail.com>
2010-05-18 23:21:48 -0400

    updating tests

--- tests/index.js
+++ tests/index.js
@@ -6,40 +6,62 @@ var sys = require('sys');
 
 var format = require('../index');
 
+function isNumber( n ){
+  if(n.toString() == 'NaN'){
+    return [false, 'NaN, nananana!'];  
+  }
+  if(n instanceof Number){
+    return [true, 'WIN!'];  
+  }
+  else{
+    return [false, n.toString() + ' is not a number.'];  
+  }
+}
+
 vows.describe('format.js library').addVows({
   "toNumber()": {
     "on an instance of Number": {
     topic: new Number( 42 ),
     "can format strict number":function( n ){
-      assert.isNumber( format.number.toNumber( n ) );
+      var result = isNumber( format.number.toNumber( n ) );
+      assert.ok( result[0], result[1] );
     }},
     "on a number literal": {
     topic: 42,
     "can format loose number":function( n ){
-      assert.isNumber( format.number.toNumber( n ) );
+      var result = isNumber( format.number.toNumber( n ) );
+      assert.ok( result[0], result[1] );
     }},
     "on a number string": {
     topic: '42',
     "can format string as number":function( n ){
-      assert.isNumber( format.number.toNumber( n ) );
+      var result = isNumber( format.number.toNumber( n ) );
+      assert.ok( result[0], result[1] );
     }},
     "on not a number at all": {
     topic: 'foobar',
     "can format garbage as number":function( n ){
-      assert.isNumber( format.number.toNumber( n ) );
+      //assert.isNumber( format.number.toNumber( n ) );
+      var result = isNumber( format.number.toNumber( n ) );
+      assert.ok( result[0], result[1] );
     }},
     "on a number with commas": {
     topic: '1,000,000',
     "can format string with commas as number":function( n ){
-      assert.isNumber( format.number.toNumber( n ) );
+      var result = isNumber( format.number.toNumber( n ) );
+      assert.ok( result[0], result[1] );
     }},
     "on a number with currency symbols": {
     topic: '$42.00',
     "can format money string as number":function( n ){
-      assert.isNumber( format.number.toNumber( n ) );
+      var result = isNumber( format.number.toNumber( n ) );
+      assert.ok( result[0], result[1] );
     }}
   }
 });
+
+
+sys.puts(format.number.toNumber('42,000'));
 
 /*
 
@@ -66,18 +88,3 @@ topic: '42',
 
 */
 
-
-var n = 45;
-
-sys.puts(typeof n);
-sys.puts(n instanceof Number);
-
-var m = Number(45);
-
-sys.puts(typeof m);
-sys.puts(m instanceof Number);
-
-var o = new Number(45);
-
-sys.puts(typeof o);
-sys.puts(o instanceof Number);
