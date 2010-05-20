df36fb80d6bfcf781af9b32217007430a5fdf917
Marak Squires <marak.squires@gmail.com>
2010-05-20 00:36:03 -0400

    a commit that should probaly be a stash

--- tests/types.js
+++ tests/types.js
@@ -57,5 +57,67 @@ vows.describe('format.js lib/types').addVows({
         }
       }
     }
+  },
+  "isString()": {
+    "on a string instance": {
+      topic: new String('foo'),
+      "this is a string":function( s ){
+        var result = format.types.isString( s );
+        if( result ){
+          assert.ok( true );
+        }
+        else{
+          assert.ok( false, '"' + s + '"' + ' is a string');
+        }
+      }
+    },
+    "on a string literal ": {
+      topic: 'foo',
+      "this is not a string":function( s ){
+        var result = format.types.isString( s );
+        if( !result ){
+          assert.ok( true );
+        }
+        else{
+          assert.ok( false, '"' + s + '"' + ' is a string');
+        }
+      }
+    },
+    "on a number literal": {
+      topic: 1234,
+      "this is not a number":function( s ){
+        var result = format.types.isString( s );
+        if( !result ){
+          assert.ok( true );
+        }
+        else{
+          assert.ok( false, '"' + s + '"' + ' is not a string');
+        }
+      }
+    },
+    "on an array": {
+      topic: new Array(1,2,3),
+      "this is not a string":function( s ){
+        var result = format.types.isString( s );
+        if( result ){
+          assert.ok( true );
+        }
+        else{
+          assert.ok( false, '"' + s + '"' + ' is not a string');
+        }
+      }
+    },
+    "on an object": {
+      topic: {},
+      "this is not a string":function( s ){
+        var result = format.types.isString( s );
+        if( result ){
+          assert.ok( true );
+        }
+        else{
+          assert.ok( false, '"' + s + '"' + ' is not a string');
+        }
+      }
+    }
   }
 });
