c5db25c22e1e6fe3d9e7331c9a684ad1008ffd93
Marak Squires <marak.squires@gmail.com>
2010-05-22 03:09:32 -0400

    updating docs

--- BUILD/docs.js
+++ BUILD/docs.js
@@ -1,5 +1,4 @@
-#  learn javascript-fu today !
-##monkey punch variables with ninja like precision 
+#  learn javascript-fu today and monkey punch your variables with ninja like precision!
 <img src = "http://imgur.com/32UFx.jpg" border = "0">
 
 ## API
