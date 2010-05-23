37258a2af075f3696f9e8e5a63dc050841c59b0f
Marak Squires <marak.squires@gmail.com>
2010-05-23 18:17:10 -0400

    updaing docs

--- BUILD/docs.js
+++ BUILD/docs.js
@@ -6,25 +6,25 @@
 
 ### isFu - the art of the curious type checker
 
-isFu methods such as isDefined() and isNumber() will accept <em>anything</em> as an argument and gracefully return true or false depending on if the arguments match the type you have checked for
+isFu methods will accept <em>anything</em> as an argument and gracefully return true or false depending on if the arguments match the type you have checked for
 
 {{{isFu}}}
 
 ### toFu - the deadly art of the monkey punch
 
-toFu methods such as toDate(), toNumber(), toPlural(), toOrdinal(), toLink() will accept <em>anything</em> as an argument and aggressively attempt to coerce the value into the type you have specified 
+toFu methods will accept <em>anything</em> as an argument and aggressively attempt to coerce the value into the type you have specified 
 
 {{{toFu}}}
 
 ### formatFu - format masking, the shadow art
 
-formatFu methods such as formatDate(), formatNumber(), formatText() will take two arguments. a value, and a mask. the mask is liberally applied to the value and returned as a string
+formatFu methods will take two arguments. a value, and a mask. the mask is liberally applied to the value and returned as a string
 
 {{{formatFu}}}
 
 ### getFu - the art of the swift getter
 
-getFu methods such as getDay(), getMonth(), getMinutes() mimic existing getters
+getFu methods take no arguments and return information based on what you asked for
 
 {{{getFu}}}
 
